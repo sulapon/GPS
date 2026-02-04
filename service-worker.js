c61b263105dfad64c8a410b6b9c8b9e5a39a44d7
sulapon <41066885+sulapon@users.noreply.github.com>
2026-02-04 16:20:45 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,23 @@
+const CACHE_NAME = 'gps-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './app.html',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png',
+  './icon-1024x1024.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('v1').then(cache => cache.addAll([
-      '/Gps-Cable-Loss-Calculator/index.html',
-      '/Gps-Cable-Loss-Calculator/manifest.json',
-      '/Gps-Cable-Loss-Calculator/icon-192x192.png',
-      '/Gps-Cable-Loss-Calculator/icon-512x512.png',
-      '/Gps-Cable-Loss-Calculator/icon-1024x1024.png'
-    ]))
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+
+
+
